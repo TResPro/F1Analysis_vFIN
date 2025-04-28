8fd716297382ce269b5b156f710e58854cb0f1bc
Tommaso Armenti <152431696+TResPro@users.noreply.github.com>
2025-04-28 12:16:22 +0200

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import io
 
 # Visualize plots and download button
 def show_fig_with_download(title, fig, filename):
-    cols = st.columns([6, 1])
+    cols = st.columns([1, 1])
     with cols[0]:
         st.subheader(title)
     with cols[1]:
