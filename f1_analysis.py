5def1cfc3b3a6824eb4eb905affd96e4b06a335c
Tommaso Armenti <152431696+TResPro@users.noreply.github.com>
2025-05-01 20:52:40 +0200

    Add files via upload

--- f1_analysis.py
+++ f1_analysis.py
@@ -55,21 +55,26 @@ def load_session(mode, year, grand_prix, session_type):
             return None
 
         try:
-            # Retrieve the event schedule for the specified year
-            event = fastf1.get_session(year,grand_prix,session_type)
+            #Check if the Grand Prix exists in that year
+            #schedule = fastf1.get_event_schedule(int(year))
+            #matched_events = schedule[schedule['EventName'].str.contains(grand_prix, case=False, na=False)]
 
-            if event is None:
-                st.warning(f"'{grand_prix}' was not held in {year}.")
-                return None
+            #if matched_events.empty:
+                #st.warning(f"{grand_prix} GP was not held in {year}.")
+                #return None
 
-            # Attempt to load the specified session
-            try:
-                session = fastf1.get_session(int(year), grand_prix, session_mapping[session_type])
-                session.load()
-                return session
-            except Exception as e:
-                st.warning(f"{session_type} was not held during the {grand_prix} weekend in {year}.")
+            # Check if session was held in that weekend
+            event = fastf1.get_event(int(year), grand_prix)
+            available_sessions = event.get_sessions()
+
+            if session_mapping[session_type] not in available_sessions:
+                st.warning(f"{session_type} was not held during the {event['EventName']} weekend in {year}.")
                 return None
+                
+            # Load the session
+            session = fastf1.get_session(int(year), grand_prix, session_mapping[session_type])
+            session.load()
+            return session
 
         except Exception as e:
             st.warning(f"Error loading session: {e}")
