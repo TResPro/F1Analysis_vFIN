f8d2930090ed17ebbf1e05d722bf0866f1ce6004
Tommaso Armenti <152431696+TResPro@users.noreply.github.com>
2025-05-01 20:58:48 +0200

    Add files via upload

--- f1_analysis.py
+++ f1_analysis.py
@@ -65,9 +65,9 @@ def load_session(mode, year, grand_prix, session_type):
 
             # Check if session was held in that weekend
             event = fastf1.get_event(int(year), grand_prix)
-            available_sessions = event['Session*']
+            available_sessions = [session.name for session in event.sessions]
 
-            if session_mapping[session_type] not in available_sessions:
+            if session_type not in available_sessions:
                 st.warning(f"{session_type} was not held during the {event['EventName']} weekend in {year}.")
                 return None
                 
